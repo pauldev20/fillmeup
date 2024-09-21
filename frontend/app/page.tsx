fd0f92041780a4bb19b25d00212dba3e355b1b05
Paul Geeser <commit@pauldev.sh>
2024-09-21 12:42:39 +0800

    basic frontend version

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,102 +1,79 @@
+"use client";
+
+import ConnectButton from "@/components/w3button";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import SpeechBubble from "@/components/speechbubble";
+import { useEffect, useState } from "react";
+// import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [speachText, setSpeachText] = useState("Hello my name is Gassy!");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <w3m-button/>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    setTimeout(() => {
+      setSpeachText("I'm here to help you with your gas fees!");
+    }, 1000
+    );
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <header className="flex flex-col gap-4 items-end w-full absolute p-4">
+        <ConnectButton/>
+      </header>
+      <main className="flex items-center justify-center min-h-screen w-full">
+        <div className="flex flex-col gap-5">
+
+          <div className="flex gap-3">
+
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-3">
+                <h1 className="flex items-center">
+                  <span className="text-4xl mr-2">①</span>Swap to get some WETH
+                </h1>
+                <div className="flex justify-center items-center">
+                  <Button>Approve WETH</Button>
+                </div>
+              </div>
+              <hr className="border-t-2 border-gray-300 mx-12"/>
+              <div className="flex flex-col gap-3">
+                <h1 className="flex items-center">
+                  <span className="text-4xl mr-2">②</span>Approve WETH for your gas on all Chains
+                </h1>
+                <Input placeholder="Enter WETH amount" />
+                <div className="flex justify-center items-center gap-4">
+                  <Button>Approve WETH</Button>
+                  <p><span className="font-bold">77,5</span> WETH left for gas</p>
+                </div>
+              </div>
+              <hr className="border-t-2 border-gray-300 mx-12"/>
+              <div className="flex flex-col gap-3">
+                <h1 className="flex items-center">
+                  <span className="text-4xl mr-2">③</span>Enjoy the gas on all the chains
+                </h1>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              <div className="relative">
+                <Image src="/noun.png" width={250} height={250} alt="Nouns Character" />
+                <SpeechBubble
+                  text={speachText}
+                  className="absolute left-full top-0 w-60 -ml-12 mt-20 transform -translate-y-full"
+                />
+              </div>
+            </div>
+
+          </div>
+
+          <hr className="border-t-2 border-gray-300 mx-3"/>
+          <div>
+            <h1 className="text-center text-lg font-bold">Transactions</h1>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
